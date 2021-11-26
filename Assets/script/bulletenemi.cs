b893e0cfb33515f399e99b7ec62167d8ad51e4de
Anoroc <l.corona@etudiant.ecolemelies.fr>
2021-11-26 15:37:41 +0100

    projrctiles marchent
    les projectiles marchent et disparaissent en sortant de l'ecran

--- Assets/script/bulletenemi.cs
+++ Assets/script/bulletenemi.cs
@@ -1,4 +1,3 @@
-using UnityEngine.InputSystem;
 using UnityEngine;
 
 public class bulletenemi : MonoBehaviour
@@ -6,33 +5,23 @@ public class bulletenemi : MonoBehaviour
     [SerializeField] float speed;
     Rigidbody2D rb;
     public int damage = 40;
-    private Transform player;
-    private Vector2 target;
-
-
+    public player player;
+    Vector2 moveDirection;
     void Start()
     {
         rb = this.GetComponent<Rigidbody2D>();
-        rb.velocity = transform.up * speed;
-        player = GameObject.FindGameObjectWithTag("Player").transform;
-        target = new Vector2(player.position.x, player.position.y);
+        player = GameObject.FindObjectOfType<player>();
+        moveDirection = (player.transform.position - transform.position).normalized * speed;
+        rb.velocity = new Vector2(moveDirection.x, moveDirection.y);
     }
-
-
-    private void Update()
-    {
-        transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
-    }
-
-
-
-    void OnTriggerEnter2D(Collider2D hitInfo)
-    {
-        player enemi = hitInfo.GetComponent<player>();
-        if (enemi != null)
+   
+        void OnTriggerEnter2D(Collider2D hitInfo)
         {
-            enemi.TakeDamage(damage);
+            player enemi = hitInfo.GetComponent<player>();
+            if (enemi != null)
+            {
+                enemi.TakeDamage(damage);
+            }
+            Destroy(gameObject);
         }
-        Destroy(gameObject);
-    }
 }
