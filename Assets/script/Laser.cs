01dcc801230949693564252c293f342d227e3256
Anoroc <l.corona@etudiant.ecolemelies.fr>
2022-01-16 16:17:32 +0100

    Lazer marche

--- Assets/script/Laser.cs
+++ Assets/script/Laser.cs
@@ -13,15 +13,18 @@ public class Laser : MonoBehaviour
     }
     void Update()
     {
-        RaycastHit2D hitInfo = Physics2D.Raycast(point.position, point.up);
+        lineRenderer.SetPosition(0, point.position);
+        lineRenderer.SetPosition(1, point.position + point.TransformDirection(Vector2.up) * 20f);
+        RaycastHit2D hitInfo = Physics2D.Raycast(point.position, point.up, 100f,LayerMask.GetMask("enemy") ,0);
         Enemy enemy = hitInfo.transform.GetComponent<Enemy>();
+        
         {
             if (enemy != null)
             {
                 enemy.TakeDamage(damage);
+                Debug.Log("Hit");
             }
-            lineRenderer.SetPosition(0, point.position);
-            lineRenderer.SetPosition(1, point.position + point.TransformDirection(Vector2.up) * 20f);
+            
         }
-    }   
+    }  
 }
