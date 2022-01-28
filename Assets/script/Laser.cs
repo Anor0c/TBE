9e6a26804afef43171d77f8249b3273f5414ee13
Anoroc <l.corona@etudiant.ecolemelies.fr>
2022-01-28 11:16:03 +0100

    Fix PlayerLazer

--- Assets/script/Laser.cs
+++ Assets/script/Laser.cs
@@ -16,19 +16,24 @@ public class Laser : MonoBehaviour
     {
         lineRenderer.SetPosition(0, point.position);
         lineRenderer.SetPosition(1, point.position + point.TransformDirection(Vector2.up) * 20f);
-        RaycastHit2D hitInfo = Physics2D.Raycast(point.position, point.up, 100f, LayerMask.GetMask("enemy", "LazerPlayer","KillProjectile"), 0); 
-        Enemy enemy = hitInfo.transform.GetComponent<Enemy>();
-        //Laser laser = hitInfo.transform.GetComponent<Laser>();
-        if (enemy == null)
+        RaycastHit2D hitInfo = Physics2D.Raycast(point.position, point.up, 100f, LayerMask.GetMask("enemy", "LazerPlayer","KillProjectile"), 0);
+        if (hitInfo.transform == null)
         {
-            Debug.Log("No Ennemi");
+            Debug.Log("No Hit");
             return;
         }
-        else if (enemy != null)
+        else
         {
-            enemy.TakeDamage(damage);
-            Debug.Log("Hit");
-        } 
+            Enemy enemy = hitInfo.transform.GetComponent<Enemy>();
+            //Laser laser = hitInfo.transform.GetComponent<Laser>();
+
+            if (enemy != null)
+            {
+                enemy.TakeDamage(damage);
+                Debug.Log("Hit");
+            }
+        }
+        
         /*if (laser !=null)
         {
          if (gunCrit.GunLevelUp>=5)
