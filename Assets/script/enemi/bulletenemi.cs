110ca255ba152d6b905c99b7148cb37757d04747
pep <sofia.boughriet@hotmail.com>
2022-02-22 14:03:02 +0100

    test

--- Assets/script/enemi/bulletenemi.cs
+++ Assets/script/enemi/bulletenemi.cs
@@ -5,23 +5,29 @@ public class bulletenemi : MonoBehaviour
     [SerializeField] float speed;
     Rigidbody2D rb;
     public int damage = 40;
-    public player player;
-    Vector2 moveDirection;
+    private Transform player;
+    private Vector2 cible;
     void Start()
     {
         rb = this.GetComponent<Rigidbody2D>();
-        player = GameObject.FindObjectOfType<player>();
-        moveDirection = (player.transform.position - transform.position).normalized * speed;
-        rb.velocity = new Vector2(moveDirection.x, moveDirection.y);
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        cible = new Vector2(player.position.x, player.position.y);
+
     }
-   
-        void OnTriggerEnter2D(Collider2D hitInfo)
-        {
-            player enemi = hitInfo.GetComponent<player>();
-            if (enemi != null)
-            {
-                enemi.TakeDamage(damage);
-            }
-            Destroy(gameObject);
-        }
+
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, cible, speed * Time.deltaTime);
+    }
+
+
+    void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        player enemi = hitInfo.GetComponent<player>();
+         if (enemi != null)
+         {
+             enemi.TakeDamage(damage);
+         }
+         Destroy(gameObject);
+    }
 }
