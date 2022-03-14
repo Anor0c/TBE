c49f1802d2b6f674d6b9fbcc01edfe6a9298f555
Anoroc <l.corona@etudiant.ecolemelies.fr>
2022-03-14 16:44:35 +0100

    ComparaisonInt

--- Assets/script/PlayerMode.cs
+++ Assets/script/PlayerMode.cs
@@ -4,19 +4,27 @@ using UnityEngine;
 
 public class PlayerMode : MonoBehaviour
 {
-    public int modeIndex;
+    public CharacterList CharaIndex;
+    [SerializeField]  private int modeIndex;
     gunLevelUp[] playerMode;
+
 
     void Awake()
     {
         //Logique pour avoir le choix de perso qui influe playerMode. 
+
+        modeIndex = CharaIndex.index;
+        ModeSwitch(modeIndex);
+    }
+    public void ModeSwitch(int index)
+    {
         playerMode = transform.GetComponentsInChildren<gunLevelUp>();
         foreach (gunLevelUp mode in playerMode)
         {
 
-            if (modeIndex != mode.modeIndex) 
+            if (index != mode.modeIndex)
             {
-                Debug.Log(this);
+                Debug.Log("MODE");
                 Destroy(mode.gameObject);
             }
         }
