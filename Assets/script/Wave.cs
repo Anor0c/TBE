01d9791b449ddd7237b2f22533a10fee87dca7e0
Anoroc <l.corona@etudiant.ecolemelies.fr>
2022-03-29 10:33:19 +0200

    Début Script WaveP2

--- Assets/script/Wave.cs
+++ Assets/script/Wave.cs
@@ -1,13 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class Wave : MonoBehaviour
 {
-    public GameObject en;
-    public GameObject en2;
+    public ActWave2 actWave2;
+
+    public GameObject[] enList;
+    public GameObject[] enListP2;
     public GameObject boss;
-    GameObject[] enemy;
+    GameObject[] enemyScene;
+
     private int EnemyCount;
     public int waveCount;
     public int waveToBoss;
@@ -26,20 +27,24 @@ public class Wave : MonoBehaviour
     }
     public void Spawn()
     {
-        Instantiate(en);
-        Instantiate(en2);
+        Instantiate(enList [0]);
+        Instantiate(enList[1]);
         Boss();
         waveCount++;
     }
     void Update()
     {
-        enemy = GameObject.FindGameObjectsWithTag("enemi");
-        EnemyCount = enemy.Length;
+        enemyScene = GameObject.FindGameObjectsWithTag("enemi");
+        EnemyCount = enemyScene.Length;
         if (EnemyCount == 0)
         {
             Spawn();
         }
-
-        
+        if (PlayerInputManager.instance.playerCount == 2) 
+        {
+            Debug.Log("player 2 in");
+            //il faudrait une facon de compter des ennemis seulement sur l'écran de P1, et de spawner des ennemis a P2 genre en faisant instanciate(en, en.position.xs+1000) 
+            //apres on fait la meme update qu'avec P1 mais avec un tag différent
+        }
     }
 }
