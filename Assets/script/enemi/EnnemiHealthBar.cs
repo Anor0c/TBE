2af6920d0e86b13539e3d7272091c9c1c91757ce
Anoroc <l.corona@etudiant.ecolemelies.fr>
2022-04-07 15:22:39 +0200

    FixHealthBar+Critical

--- Assets/script/enemi/EnnemiHealthBar.cs
+++ Assets/script/enemi/EnnemiHealthBar.cs
@@ -3,7 +3,12 @@ using UnityEngine.UI;
 public class EnnemiHealthBar : MonoBehaviour
 {
     public Image healthBarEnemy;
-    public Enemy enemy;
+    [HideInInspector] public Enemy enemy;
+
+    private void Start()
+    {
+        enemy = this.gameObject.GetComponentInParent<Enemy>();
+    }
     public void UpdateEnemyHealthBar()
     {
         healthBarEnemy.fillAmount = Mathf.Clamp(enemy.health / enemy.maxHealth, 0, 1f);
