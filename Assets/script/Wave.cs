338bb1ab9e525ce007a3a49b789f69ef24723892
Anoroc <l.corona@etudiant.ecolemelies.fr>
2022-04-07 10:43:25 +0200

    LaCoroutineMarcheAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- Assets/script/Wave.cs
+++ Assets/script/Wave.cs
@@ -5,18 +5,19 @@ using UnityEngine.InputSystem;
 using UnityEngine.Events;
 public class Wave : MonoBehaviour
 {
-    public Wave[] wavesInScene;
     [SerializeField] UnityEvent onPlayerJoined;
+    private Coroutine spawnWait;
 
     public GameObject[] enList;
     public GameObject boss;
     Enemy[] enemyScene;
-    private Coroutine spawnWait;
+    public Wave[] wavesInScene;
 
     private int EnemyCount;
     public int waveCount;
     public int waveToBoss;
     public int waitForSpawn;
+    public bool CoroutineIsRunning = false;
 
     public void Start()
     {
@@ -25,7 +26,6 @@ public class Wave : MonoBehaviour
             waveCount = 0;
         else
             waveCount = wavesInScene[0].waveCount;
-        Spawn();
     }
     public void OnPlayer2Joined()
     {
@@ -41,14 +41,15 @@ public class Wave : MonoBehaviour
     }
     public void Spawn()
     {
+        waveCount++;
         Instantiate(enList[0], this.gameObject.transform);
         Instantiate(enList[1], this.gameObject.transform);
         Boss();
-        waveCount++;
     }
     private IEnumerator SpawnWait()
     {
-        new WaitForSeconds(waitForSpawn);
+        CoroutineIsRunning = true;
+        yield return new WaitForSeconds(waitForSpawn);
         Spawn();
         yield return null;
     }
@@ -58,16 +59,24 @@ public class Wave : MonoBehaviour
         EnemyCount = enemyScene.Length;
         if (EnemyCount == 0)
         {
-            if (spawnWait != null)
-            {
-                StopCoroutine(SpawnWait());
-                Debug.Log("love when it wrks");
-            }
+            if (CoroutineIsRunning == true) 
+                return;
 
-            spawnWait = StartCoroutine(SpawnWait());
+            spawnWait=StartCoroutine(SpawnWait());
             //Spawn();
             //il faudrait une coroutine qui s'invoque une seule fois pour lancer un wait de genre 2 secondes avant de lancer  
+            //je peut peut etre untiliser wavecount pour lancer la coroutine une seule fois
         }
+        else
+        {
+            if (spawnWait != null)
+            {
+                StopCoroutine(spawnWait);
+                CoroutineIsRunning = false;
+                Debug.Log("pls stop");
+            }
+        }
+
         if (PlayerInputManager.instance.playerCount == 2)
             OnPlayer2Joined();   
     }
