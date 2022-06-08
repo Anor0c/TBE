7fb9a6a0d2f10ab78d521aed0d2576740f818a8b
Anoroc <l.corona@etudiant.ecolemelies.fr>
2022-06-08 14:31:17 +0200

    LilFixEnnemy

--- Assets/script/enemi/Enemy.cs
+++ Assets/script/enemi/Enemy.cs
@@ -50,6 +50,7 @@ public class Enemy : MonoBehaviour
             }
             Destroy(this.gameObject);
         }
-        bossWin.BossDied();
+        else
+            bossWin.BossDied();
     }
 }
