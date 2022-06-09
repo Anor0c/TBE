1b3ce5c93afaef9e990865a1145093d506b4dabc
Anoroc <l.corona@etudiant.ecolemelies.fr>
2022-06-09 18:00:49 +0200

    P2Fixed

--- Assets/script/enemi/bulletenemi.cs
+++ Assets/script/enemi/bulletenemi.cs
@@ -1,18 +1,38 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class bulletenemi : MonoBehaviour
 {
-    [SerializeField] float speed;
-    Rigidbody2D rb;
+    [SerializeField] private float speed;
+    public PlayerMode[] playerList;
     public float damage = 40;
+
     [HideInInspector] public PlayerMode player;
-    Vector2 moveDirection;
+    private Rigidbody2D rb;
+    private Vector2 moveDirection;
     void Start()
     {
         rb = this.GetComponent<Rigidbody2D>();
-        player = GameObject.FindObjectOfType<PlayerMode>();
-        moveDirection = (player.transform.position - transform.position).normalized * speed;
-        rb.velocity = new Vector2(moveDirection.x, moveDirection.y);
+        playerList = FindObjectsOfType<PlayerMode>();
+
+        if (PlayerInputManager.instance.playerCount == 0) 
+            rb.velocity = Vector2.down * speed;
+        else
+        {
+            if (PlayerInputManager.instance.playerCount == 1) 
+                player = playerList[0];
+
+            if (PlayerInputManager.instance.playerCount >= 2) 
+            {
+                if (transform.position.x > 800)
+                    player = playerList[0];
+                else
+                    player = playerList[1];
+            }
+            moveDirection = (player.transform.position - transform.position).normalized * speed;
+            rb.velocity = new Vector2(moveDirection.x, moveDirection.y);
+        }
+        Debug.Log(player);
     }
 
     void OnTriggerEnter2D(Collider2D hitInfo)
