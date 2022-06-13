5a096f69ae12283005e5f7094b60429ac3752c87
pep <sofia.boughriet@hotmail.com>
2022-06-13 09:20:17 +0200

    mood start

--- Assets/script/HealthBehaviour.cs
+++ Assets/script/HealthBehaviour.cs
@@ -6,6 +6,11 @@ public class HealthBehaviour : MonoBehaviour
     public float maxHealth = 500;
     public float health;
 
+    public SpriteRenderer spriteRenderer;
+    public Sprite high;
+    public Sprite mid;
+    public Sprite low;
+
     public HealthBar healthBar;
     [SerializeField] UnityEvent onPlayerDied;
 
@@ -15,6 +20,15 @@ public class HealthBehaviour : MonoBehaviour
         health -= damage;
 
         healthBar.UpdateHealthBar();
+
+        if (health < 500)
+            spriteRenderer.sprite = high;
+
+        if (health < 300)
+            spriteRenderer.sprite = mid;
+
+        if (health < 200)
+            spriteRenderer.sprite = low;
 
         if (health <= 0)
             PlayerDied();
